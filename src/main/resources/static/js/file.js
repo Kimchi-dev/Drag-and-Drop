ec97972e57d3b42fedbe7e9015fbd6b691d2f721
Kimchi Developer <65699391+Kimchi-dev@users.noreply.github.com>
2021-04-21 17:59:51 +0900

    file drop success

--- src/main/resources/static/js/file.js
+++ src/main/resources/static/js/file.js
@@ -1,21 +1,35 @@
 
-//dragEnter
-const dragEnterHandler = (e) => {
-    console.log("enterd file")
+$(function(){
+    var dropZone = $('.sub-container')
 
-    $(".sub-container").addClass("draged")
-}
+    dropZone.on('dragenter', e => {
+        e.stopPropagation()
+        e.preventDefault()
 
-//dragLeave
-const dragLeaveHandler = (e) => {
+        $(".sub-container").addClass('draged')
+    })
 
-    $(".sub-container").removeClass("draged")
-}
+    dropZone.on('dragleave', e => {
+        e.stopPropagation()
+        e.preventDefault()
+        $(".sub-container").removeClass('draged')
+    })
+    dropZone.on('dragover', e => {
+        e.stopPropagation()
+        e.preventDefault()
+        $(".sub-container").addClass('draged')
+    })
+    dropZone.on('drop', e => {
+        e.preventDefault()
 
-const dropHandler = (e) => {
-    e.preventDefault()
-    $(".sub-container").removeClass("draged")
+        $(".sub-container").removeClass('draged')
 
-    var files = e.originalEvent.dataTransfer.files
-    console.log(files)
-}
+        var files = e.originalEvent.dataTransfer.files
+        console.log(files)
+
+    })
+})
+
+
+
+
